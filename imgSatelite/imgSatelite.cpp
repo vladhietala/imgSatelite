56e7d95a562a03e08c4202bf586164a782e9ccc8
Vlad Hietala <vlad.hietala@hotmail.com>
2016-03-15 20:23:38 -0300

    Update imgSatelite.cpp

--- imgSatelite/imgSatelite.cpp
+++ imgSatelite/imgSatelite.cpp
@@ -1,53 +1,71 @@
-// imgSatelite.cpp : Defines the entry point for the console application.
-//
+#include <stdio.h>
+#include <tchar.h>
+#include <stdlib.h>
 
-#include "stdafx.h"
-#include "imgSatelite.h"
+void buscaConstrucao(char **, int, int);
+int buscaLista(char **, int, int);
 
-int buscaConstrucao(char **, size_t, size_t);
-
+int sizeX, sizeY, numConstr;
 int main()
 {
-	char **img;
+    char **img;
 
-	int sizeX = 9, sizeY = 15;
+    sizeX = 9;
+    sizeY = 15;
+    int i, j;
 
-	img = (char **)malloc(sizeX * sizeof(char *));
+    numConstr = 0;
+    img = (char **)malloc(sizeX * sizeof(char *));
 
-	for (size_t i = 0; i < sizeY; i++)
-	{
-		img[i] = (char *)malloc(sizeY * sizeof(char *));
-	}
+    for (i = 0; i < sizeY; i++)
+    {
+        img[i] = (char *)malloc(sizeY * sizeof(char *));
+    }
 
-	img[0] = "000000000000000";
-	img[1] = "007677888000080";
-	img[2] = "008000000900000";
-	img[3] = "006003300080000";
-	img[4] = "005003300800000";
-	img[5] = "004000009000600";
-	img[6] = "034556780005000";
-	img[7] = "300000000004400";
-	img[8] = "020000000000000";
+    img[0] = "000000000000000";
+    img[1] = "007677888000080";
+    img[2] = "008000000900000";
+    img[3] = "006003300080000";
+    img[4] = "005003300800000";
+    img[5] = "004000009000600";
+    img[6] = "034556780005000";
+    img[7] = "300000000004400";
+    img[8] = "020000000000000";
 
-	//img = img2;
+    //img = img2;
 
-	for (size_t i = 0; i < sizeX; i++)
-	{
-		for (size_t j = 0; j < sizeY; j++)
-		{
-			int r = buscaConstrucao(img, i, j);
-		}
+    for ( i = 0; i < sizeX; i++)
+    {
+        for ( j = 0; j < sizeY; j++)
+        {
+            if (img[i][j] > 0)
+                buscaConstrucao(img, i, j);
+        }
 
-	}
-
-	getchar();
-
-	return 0;
+    }
+    printf("%d objetos",numConstr);
+    getchar();
+    return 0;
 }
 
-int buscaConstrucao(char ** img, size_t i, size_t j) {
-	int pos = 0;
-	fputchar(img[i][j]);
-	return pos;
+void buscaConstrucao(char ** img, int i, int j){
+    if (i > 0 && i < sizeX){
+        if (buscaLista(img, i - 1, j))
+            return;
+        if (j>0 && j< sizeY){
+          if (buscaLista(img, i - 1, j -1))
+            return;
+          if (buscaLista(img, i, j - 1))
+            return;
+          if (buscaLista(img, i + 1, j))
+            return;
+        }
+    }
+    numConstr++;
+    return;
+}
+
+int buscaLista(char ** img, int i, int j){
+    return (img[i][j] > 0) ? 1 : 0;
 }
 
